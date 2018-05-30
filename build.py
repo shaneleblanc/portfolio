60869dc6cae2b604f5194ed2b92051f6acfd7ed6
Shane LeBlanc <shane@xs.vc>
2018-05-30 16:37:06 -0700

    Refactored build script for base.html

--- build.py
+++ build.py
@@ -1,23 +1,53 @@
+pages = [
+    {
+        'filename': 'content/index.html',
+        'title': 'Home'
+    },
+    {
+        'filename': 'content/about.html',
+        'title': 'About',
+    },
+    {
+        'filename': 'content/blog.html',
+        'title': 'Blog',
+    },
+    {
+        'filename': 'content/contact.html',
+        'title': 'Contact',
+    },
+    {
+        'filename': 'content/services.html',
+        'title': 'Services',
+    },
+    {
+        'filename': 'content/audio.html',
+        'title': 'Audio',
+    },
+    {
+        'filename': 'content/music.html',
+        'title': 'Music',
+    },
+    {
+        'filename': 'content/software.html',
+        'title': 'Software',
+    },
+    {
+        'filename': 'content/hardware.html',
+        'title': 'Hardware',
+    },
+]
+
 def main():
     import os
     import fileinput
-    #Make sure top and bottom exist and are files...
-    try:
-        with open('templates/top.html') as file, open('templates/bottom.html') as file:
-            pass
-    except IOError as e:
-        print(e)
-    top = open('templates/top.html').read()
-    bottom = open('templates/bottom.html').read
-
-    for root, dirs, files in os.walk('content/'):
-        for file in files:
-            if file.endswith('.html'):
-                print("Making " + file)
-                newfile = top + open('content/'+file).read() + bottom;
-                outfile = open('docs/'+file, 'w')
-                outfile.write(newfile)
-                outfile.close()
+    base_file = open('templates/base.html').read()
+    for page in pages:
+        content_file = open(page['filename']).read()
+        print("Making " + page['title'])
+        outfile = base_file.replace('{{title}}', page['title'])
+        outfile = outfile.replace('{{content}}', content_file)
+        outfile = outfile.replace(page['title']+'class','active')
+        open('docs/'+page['filename'].replace('content/',''), 'w').write(outfile)
     print("Succesfully added files to docs/")
 
 if __name__ == '__main__':
